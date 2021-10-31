6440156cfe9380ca2510e096fe2656dbbec3eff5
Eduardo <eduardo.coria@protonmail.com>
2021-10-31 20:06:41 +0000

    adding node, docker, ...

--- after_install.sh
+++ after_install.sh
@@ -4,7 +4,7 @@
 apt update -y
 upgrade -y
 
-apt_packages=(vim unzip gimp bind9-utils awscli terminator chromium-browser curl tree net-tools dia simplescreenrecorder imagemagick vlc mysql-client nmap htop git swaks tcpdump virtualbox vagrant mesa-utils flameshot)
+apt_packages=(vim unzip gimp bind9-utils awscli terminator chromium-browser curl tree net-tools dia simplescreenrecorder imagemagick vlc mysql-client nmap htop git swaks tcpdump virtualbox vagrant docker docker-compose npm nodejs telegram-desktop mesa-utils neofetch flameshot gnome-tweak-tool)
 
 snap_packages=(code kontena-lens postman teams slack kubectl)
 
